f1f22924cb399269d6513e3af408034c188e5d0d
jonathanhovland <jonathan.hovland@yahoo.com>
2018-12-05 15:17:35 -0700

    you can now view books when you click on books in the navbar

--- books/src/Components/Books.js
+++ books/src/Components/Books.js
@@ -5,22 +5,14 @@ import { Card, Icon, Image } from 'semantic-ui-react'
 
 const Books = ({books}) => {
   console.log("books",books)
-  return books.map(book => {
+  return books.map((book, i) => {
     return(
-      <Card>
-        <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' />
+      <Card key={i}>
+        <Image src={book.bookCoverURL} />
         <Card.Content>
-          <Card.Header>{book}</Card.Header>
-          <Card.Meta>
-            <span className='date'>Joined in 2015</span>
-          </Card.Meta>
-          <Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <a>
-            <Icon name='user' />
-            22 Friends
-          </a>
+          <Card.Header>{book.bookTitle}</Card.Header>
+          <Card.Description>{book.bookGenre}</Card.Description>
+          <Card.Description>{book.bookDescription}</Card.Description>
         </Card.Content>
       </Card>
     )
